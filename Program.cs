1552c1207a7f2990ad3102805ea65ba644fc5f24
Mehmet Ali <101504173+MehmetAliTspnr@users.noreply.github.com>
2022-07-28 22:52:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sehir_Atama_Programı
+{
+    internal class Program//OGRETMEN SEHİR ATAMASI
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine(":::Sehir Atama Programi:::");
+            string[] sehirler = { "Ankara", "Bursa", "Adana", "Hatay", "İstanbul", "Antalya", "İzmir" };
+            Random rastgele=new Random();
+            a = rastgele.Next(0, sehirler.Length);// dizinin uzunlugu kadar maksimum değer üretecek.
+            Console.WriteLine(a+"-"+sehirler[a]);
+      
+            Console.Read();
+            
+
+        }
+    }
+}
